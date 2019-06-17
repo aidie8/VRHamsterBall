029bf0e1d71eadf2fccdb63c81ca20d3032ef368
Aiden <aidenvanzuilen@gmail.com>
2019-06-17 16:20:29 +1200

    working keyboard controls

---
+++ VRHamsterball/Assets/Hamsterball/Scripts/CubeMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    public Rigidbody ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      this.transform.position=ball.transform.position - new Vector3(0, ball.GetComponent<SphereCollider>().radius / 2);
+    }
+}
