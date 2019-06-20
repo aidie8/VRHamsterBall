48547e622928c0573646e59bb83254f3d25ed248
Aiden <aidenvanzuilen@gmail.com>
2019-06-20 14:06:36 +1200

    hand controls

---
+++ VRHamsterball/Assets/Hamsterball/Scripts/HandControls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class HandControls : MonoBehaviour
+{
+    public SteamVR_Action_Boolean grabAction;
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public Hand controller;
+    public GameObject pointerObject;
+    //private variables
+    private GameObject pointerball;
+    private bool ballGrabbed;
+    private Vector3 grabPos;
+    Vector3 prevVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ballGrabbed && getGrab()) {
+            ballGrabbed = true;
+            RaycastHit hit;
+            if (Physics.Raycast(controller.transform.position, controller.transform.TransformDirection(Vector3.forward),out hit)) {
+                Debug.Log("ray hit");
+                pointerball = Instantiate(pointerObject);
+                pointerObject.transform.position = hit.transform.position;
+            }
+        }
+
+        
+        if (!getGrab()) {
+            ballGrabbed = false;
+            Destroy(pointerObject);
+        }
+        if (ballGrabbed)
+        {
+
+        }
+    }
+
+    bool getGrab()
+    {
+        return grabAction.GetStateDown(handType);
+
+    }
+}
--- VRHamsterball/Assets/Hamsterball/Scripts/PlayerMovement.cs
+++ VRHamsterball/Assets/Hamsterball/Scripts/PlayerMovement.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 using Valve.VR.InteractionSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     public Rigidbody ball;
     public Player player;
     Player playerScript;
@@ -12,8 +14,6 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        up = Input.GetKey("a");
-        Debug.LogWarning(up);
         playerScript = player.GetComponent<Player>();
     }
 
@@ -22,11 +22,12 @@ public class PlayerMovement : MonoBehaviour
     {
         //transforms player to the ball every update,makes the player down a bit in the ball. can be ajusted to work better
         playerScript.trackingOriginTransform.position = ball.transform.position - new Vector3(0, ball.GetComponent<SphereCollider>().radius / 2);
-        if (up) {
-            Debug.Log("up was pressed");
             Vector3 dir = new Vector3(0, 0, 9);
             ball.AddRelativeTorque(dir,ForceMode.Impulse);
 
         }
     }
+
+
+
 }
