018865e610822b74e2e982df795b2c1ea5710d81
CommercePC <aidenvanzuilen@gmail.com>
2019-08-05 16:51:41 +1200

    Tried to fix movement, experimented a few things
    Nothing works so far. did learn bit though

--- VRHamsterball/Assets/Hamsterball/Scripts/Movement/HandControls.cs
+++ VRHamsterball/Assets/Hamsterball/Scripts/Movement/HandControls.cs
@@ -15,33 +15,39 @@ public class HandControls : MonoBehaviour
     public Player player;
     //private variables
     private GameObject pointerball;
-    private bool ballGrabbed;
     private Vector3 grabPos;
     // Start is called before the first frame update
     void Start()
     {
-    
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (getGrab()) {
+        if (getGrab())
+        {
             print("Trigger Pressed");
         }
-           print("triggers current state" + getTriggerState());
-        
-        if (!ballGrabbed && getGrab()) {
-            ballGrabbed = true;
+        print("triggers current state" + getTriggerState());
+
+        if (getGrab())
+        {
             RaycastHit hit;
-            if (Physics.Raycast(controller.transform.position, controller.transform.TransformDirection(Vector3.forward), out hit))
+            Ray forward = new Ray(controller.transform.position, controller.transform.forward);
+            forward.origin = forward.GetPoint(2);
+            forward.direction = -forward.direction;
+            Debug.DrawLine(forward.origin,controller.transform.position);
+            pointerObject.transform.position = forward.origin;
+            print(forward.origin);
+            if (Physics.Raycast(forward, out hit))
             {
+                print(hit.transform.gameObject);
                 if (hit.transform.gameObject == ball)
                 {
                     Debug.Log("ray hit");
                     pointerObject.transform.position = hit.transform.position;
-
                     this.GetComponent<FixedJoint>().connectedBody = ball.GetComponent<Rigidbody>();
                     this.GetComponent<FixedJoint>().connectedAnchor = hit.transform.gameObject.GetComponent<Rigidbody>().transform.position;
                     print("1");
@@ -50,20 +56,22 @@ public class HandControls : MonoBehaviour
                     print(this.GetComponent<FixedJoint>().connectedBody);
                 }
             }
-        }
-        if (!getGrab()) {
-            this.GetComponent<FixedJoint>().connectedBody = null;
-            this.GetComponent<FixedJoint>().connectedAnchor = new Vector3(0, 0, 0);
-            ballGrabbed = false;
-        }
-    }
-    bool getGrab()
-    {
-        return grabAction.GetStateDown(handType);
 
-    }
+            if (!getTriggerState())
+            {
+                this.GetComponent<FixedJoint>().connectedBody = null;
+                this.GetComponent<FixedJoint>().connectedAnchor = new Vector3(0, 0, 0);
+            }
+        }
+        bool getGrab()
+        {
+            return grabAction.GetStateDown(handType);
 
-    bool getTriggerState() {
-        return grabAction.GetState(handType);
+        }
+
+        bool getTriggerState()
+        {
+            return grabAction.GetState(handType);
+        }
     }
 }
