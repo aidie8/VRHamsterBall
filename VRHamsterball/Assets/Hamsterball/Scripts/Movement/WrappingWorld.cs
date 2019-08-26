e205260fd022b4779593461267ea20eb1c20e2d3
Jamie <118700@cloud.rangitoto.school.nz>
2019-08-26 16:10:13 +1200

    added wrapping world

---
+++ VRHamsterball/Assets/Hamsterball/Scripts/Movement/WrappingWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrappingWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int MinimumHeight = -50;
+    public int TeleportHeight = 100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<GameObject>().transform.position.y < MinimumHeight)
+        {
+            this.GetComponent<GameObject>().transform.position= new Vector3 (0, TeleportHeight, 0);
+        }
+            
+    }
+}
