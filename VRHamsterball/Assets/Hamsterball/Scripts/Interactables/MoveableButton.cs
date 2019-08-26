bf8b0b65ab0eac919504e797dc5a32310b3f69c4
Aiden <aidenvanzuilen@gmail.com>
2019-08-26 15:33:49 +1200

    Added stuff

--- VRHamsterball/Assets/Hamsterball/Scripts/Interactables/MoveableButton.cs
+++ VRHamsterball/Assets/Hamsterball/Scripts/Interactables/MoveableButton.cs
@@ -1,20 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MoveableButton : MonoBehaviour
 {
-    public enum Direction { x, y, z };
-    public Direction dir;
+    public UnityEvent ButtonPressed;
+    public float maxOffset = .6f;
+    //public bool ReverseDirection;
+    private Vector3 intialPos;
+    private bool pressed = false;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
+        intialPos = this.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        double diff = Math.Round((Mathf.Abs(transform.localPosition.y - intialPos.y)),3);
+        print("max offset = " + maxOffset + " diff " + diff + " pressed = " + !pressed);
+        //print(diff);
+        if (diff > maxOffset && !pressed)
+        {
+            pressed = true;
+            ButtonPressed.Invoke();
+        }
+        else if  (diff < maxOffset)
+            {
+            pressed = false;
+            }
+        }
+
+
+    public bool getPressed() {
+        return pressed;
     }
 }
---
+++ VRHamsterball/Assets/Hamsterball/Scripts/Interactables/PlayerCollisionOnly.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionOnly : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player") {
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), this.GetComponent<Collider>());
+        }
+    }
+}
