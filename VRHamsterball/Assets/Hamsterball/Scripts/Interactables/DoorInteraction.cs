a306b73d0570b5b5a21b4476817a3dc623952f8a
Aiden <aidenvanzuilen@gmail.com>
2019-09-09 16:54:42 +1200

    Door interaction script

---
+++ VRHamsterball/Assets/Hamsterball/Scripts/Interactables/DoorInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteration : MonoBehaviour
+{
+    S
+    public void OpenDoor() {
+        this.enabled = false;
+    }
+
+    public void CloseDoor() {
+        this.enabled = true;
+    }
+}
