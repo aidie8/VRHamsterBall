802df4f201b69bd530e34a2498451c878605b86c
Aiden <Aidenvanzuilen@gmail.com>
2019-11-07 18:19:55 +1300

    Cleanup, start of level design

---
+++ VRHamsterball/Assets/Hamsterball/Resources/Scripts/Interactables/VRPresentCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.XR;
+
+public class VRPresentCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isenabled = true;
+    public GameObject todisable;
+    void Start()
+    {
+        if (!XRDevice.isPresent) {
+            todisable.SetActive(false);
+            isenabled = false;
+        }
+    }
+}
