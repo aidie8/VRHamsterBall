28c2758e3843269bf9e4b8859ed7140321a21bca
Aiden <Aidenvanzuilen@gmail.com>
2019-11-13 21:17:55 +1300

    More stuff added to the scene %50ish  complete.
    just need to add the finer details and stuff

--- VRHamsterball/Assets/Hamsterball/Resources/Scripts/Interactables/MoveableButton.cs
+++ VRHamsterball/Assets/Hamsterball/Resources/Scripts/Interactables/MoveableButton.cs
@@ -8,6 +8,7 @@ public class MoveableButton : MonoBehaviour
 {
     public UnityEvent ButtonPressed;
     public UnityEvent ButtonReleased;
+    private double heighestoffset = 0;
     public float maxOffset = .6f;
     //public bool ReverseDirection;
     private Vector3 intialPos;
@@ -23,7 +24,11 @@ public class MoveableButton : MonoBehaviour
     {
         
         double diff = Math.Round((Mathf.Abs(transform.localPosition.y - intialPos.y)),3);
-        //print(diff);
+        if (diff > heighestoffset) {
+            heighestoffset = diff;
+            print(heighestoffset);
+        }
+        
         if (diff > maxOffset && !pressed)
         {
             pressed = true;
---
+++ VRHamsterball/Assets/Hamsterball/Resources/Scripts/Interactables/OpenDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    Animator Animator;
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DoorOpen()
+    {
+        Animator.Play("open");
+
+    }
+}
