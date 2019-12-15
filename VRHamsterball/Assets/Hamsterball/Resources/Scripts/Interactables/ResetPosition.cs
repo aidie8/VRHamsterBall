fbefda711e09138c10562906b6bdb440c24406a5
Aiden <aidenvanzuilen@gmail.com>
2019-12-16 10:48:26 +1300

    reset position script

---
+++ VRHamsterball/Assets/Hamsterball/Resources/Scripts/Interactables/ResetPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+
+    private Vector3 StartPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void ResetPostion() {
+        this.transform.position = StartPosition;
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+    }
+}
