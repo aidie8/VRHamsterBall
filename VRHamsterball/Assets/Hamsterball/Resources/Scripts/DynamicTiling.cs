2bbf91dace1e27dd2e6963b1a99ab47c367aa4b2
Aiden <Aidenvanzuilen@gmail.com>
2019-12-17 22:18:26 +1300

    Dynamic tiling textures, Walls looking better

---
+++ VRHamsterball/Assets/Hamsterball/Resources/Scripts/DynamicTiling.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicTiling : MonoBehaviour
+{
+    [SerializeField] private float tileX = 1;
+    [SerializeField] private float tileY = 1;
+    Mesh mesh;
+    private Material mat;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        mesh = GetComponent<MeshFilter>().mesh;
+
+    }
+
+    void Update()
+    {
+        if (transform.localScale.z > transform.localScale.x)
+        {
+            mat.mainTextureScale = new Vector2((mesh.bounds.size.z *
+            transform.localScale.z)/10 * tileX, (mesh.bounds.size.y * transform.localScale.y)/10 * tileY);
+        }
+        else {
+            mat.mainTextureScale = new Vector2((mesh.bounds.size.x *
+            transform.localScale.x)/10 * tileX, (mesh.bounds.size.y * transform.localScale.y)/10 * tileY);
+        }
+    }
+}
