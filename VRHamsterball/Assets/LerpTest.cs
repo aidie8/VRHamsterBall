a5def5dca19dd127deec79413c9506a3c6a18ecd
Aiden <aidenvanzuilen@gmail.com>
2020-02-27 14:37:30 +1300

    Stuff testing rotating doors and stuff (drawbridge?))

---
+++ VRHamsterball/Assets/LerpTest.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpTest : MonoBehaviour
+{
+    public double speed = 0.5d;
+    public Vector3 endpos;
+    private double time;
+    private Vector3 startpos;
+    private Vector3 origin;
+    private float distance;
+    private bool pressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = this.transform.position;
+        origin = startpos;
+        endpos = startpos + new Vector3(0, 4, 0);
+        time = Time.time;
+        distance = Vector3.Distance(startpos, endpos);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(pressed);
+        if (pressed)
+        {
+            transform.position = Vector3.Lerp(this.transform.position, endpos, (float)getFraction());
+        }
+        else {
+            transform.position = Vector3.Lerp(this.transform.position, startpos, (float)getFraction());
+        }
+        if (getFraction() >= 1f) {
+            resetTime();
+        }
+
+    }
+
+
+    public void buttonpressed()
+    {
+        pressed = true;
+    }
+
+    public void buttonrelease()
+    {
+        pressed = false;
+
+    }
+
+    void resetTime()
+    {
+
+        time = Time.time;
+    }
+
+    double getDistanceCovered() {
+        return (Time.time - time) * speed;
+
+
+    }
+
+    double getFraction() {
+        return getDistanceCovered() / distance;
+    }
+    
+
+    public void onTargetchange() {
+        resetTime();
+        setStartPos();
+    }
+
+    public void SetEndPos(int num) {
+        onTargetchange();
+        switch (num) {
+
+
+            case 0:
+                endpos = origin;
+                break;
+            case 1:
+               endpos = origin + new Vector3(4,0,0);
+                break;
+            case 2:
+                endpos = origin + new Vector3(4, 4, 0);
+                break;
+            case 3:
+                endpos = origin + new Vector3(0, 4, 0);
+                break;
+            default:
+                endpos = origin;
+                break;
+                
+        }
+       
+
+    }
+
+   
+
+
+    public void setStartPos() {
+        startpos = this.transform.position;
+    }
+}
---
+++ VRHamsterball/Assets/RotateLerpTest.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLerpTest : MonoBehaviour
+{
+    public double speed = 0.5d;
+    public Vector3 endpos;
+    private double time;
+    private Vector3 startpos;
+    private Vector3 originpos;
+    private Quaternion startangle;
+    private Quaternion endangle;
+    private float distance;
+    private bool pressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = this.transform.position;
+        originpos = startpos;
+        endpos = startpos + new Vector3(-10, -10, 0);
+        time = Time.time;
+        distance = Vector3.Distance(startpos, endpos);
+        startangle = this.transform.rotation;
+        endangle = new Quaternion(0,0,-90,0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(pressed);
+        if (pressed)
+        {
+            transform.position = Vector3.Lerp(this.transform.position, endpos, (float)getFraction());
+            transform.rotation = Quaternion.Lerp(this.transform.rotation, endangle, (float)getFraction());
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(this.transform.position, startpos, (float)getFraction());
+            transform.rotation = Quaternion.Lerp(this.transform.rotation,startangle, (float)getFraction());
+        }
+        if (getFraction() >= 1f)
+        {
+            resetTime();
+        }
+
+    }
+
+
+    public void buttonpressed()
+    {
+        pressed = true;
+    }
+
+    public void buttonrelease()
+    {
+        pressed = false;
+
+    }
+
+    void resetTime()
+    {
+
+        time = Time.time;
+    }
+
+    double getDistanceCovered()
+    {
+        return (Time.time - time) * speed;
+
+
+    }
+
+    double getFraction()
+    {
+        return getDistanceCovered() / distance;
+    }
+
+
+    public void onTargetchange()
+    {
+        resetTime();
+        setStartPos();
+    }
+
+    public void SetEndPos(int num)
+    {
+        onTargetchange();
+        switch (num)
+        {
+
+
+           
+        }
+
+
+    }
+
+
+
+
+    public void setStartPos()
+    {
+        startpos = this.transform.position;
+    }
+
+    public void setStartRotation() {
+        
+    }
+}
+
